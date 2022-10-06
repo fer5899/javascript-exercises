436e84b2c6fe90fbcb0e8c951b9a330ffe6a1d3c
fer5899 <fdometallic@gmail.com>
2022-10-06 17:29:06 +0200

    Finish exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    people.sort((a,b) => {
+        let currYear = new Date().getFullYear();
+        let aDeath = a.yearOfDeath;
+        if(typeof a.yearOfDeath === "undefined") aDeath = currYear;
+        let bDeath = b.yearOfDeath;
+        if(typeof b.yearOfDeath === "undefined") bDeath = currYear;
 
+        const aAge = aDeath - a.yearOfBirth;
+        const bAge = bDeath - b.yearOfBirth;
+
+        return aAge < bAge ? 1 : -1;
+    });
+    console.log(people);
+
+    return people[0];
 };
 
 // Do not edit below this line
