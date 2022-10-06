defafd2375442a10e2f75b6182969ccbc0d2f43b
fer5899 <fdometallic@gmail.com>
2022-10-06 16:40:25 +0200

    Finish exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
-
+const palindromes = function (exp) {
+    let expArr = exp.toLowerCase().split("");
+    let filtered = expArr.filter(item => 
+        !([","," ","!","?","."].includes(item))
+        );
+    let orig = [...filtered];
+    return arrayEquals(filtered.reverse(), orig);
 };
+
+function arrayEquals(a, b) {
+    return Array.isArray(a) &&
+      Array.isArray(b) &&
+      a.length === b.length &&
+      a.every((val, index) => val === b[index]);
+  }
 
 // Do not edit below this line
 module.exports = palindromes;
