a4fce5c399f6a6f8b0633a20f6474c670de12919
fer5899 <fdometallic@gmail.com>
2022-10-06 16:55:07 +0200

    Finish exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if(num < 0) return "OOPS";
+    if (typeof num === "string") num = parseInt(num);
+    let fibCurr = 0;
+    let fibNext = 1;
+    let temp;
+    for(let i = 0; i < num; i++) {
+        temp = fibNext;
+        fibNext = fibCurr + fibNext;
+        fibCurr = temp;
+    }
 
+    return fibCurr;
 };
 
 // Do not edit below this line
